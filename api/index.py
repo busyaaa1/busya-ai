99080ce592241edb403b0d816b5b34ad7498eeca
busyaaa_1 <malikarasulova2022@gmail.com>
2025-12-04 15:24:26 +0500

    Delete api/index.py

--- api/index.py
+++
@@ -1,7 +0,0 @@
-# api/index.py
-from app import app as application  # импортируем готовое приложение из твоего app.py
-
-# ЭТО САМАЯ ВАЖНАЯ СТРОКА ДЛЯ VERCEL
-__vercel_app__ = application
-
-# ←←←←←←←←←←←←←←←←←←←←
